91f2f42daa64d92f87309d28e8fdca52dd8e0a21
iamramnaresh <krishnaraodemudamma@gmail.com>
2024-06-14 19:18:07 +0530

    Footer Modification

--- client/src/components/Components-Footer/Footer-Renderer.tsx
+++ client/src/components/Components-Footer/Footer-Renderer.tsx
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme: Theme) =>
     footer: {
       position: 'relative',
       background: '#F5FBF5',
-      height: '100%',
+      height:"100%"
     },
   })
 );
@@ -21,13 +21,13 @@ const FooterRenderer: React.FC = () => {
 
   return (
     <footer className={classes.footer}>
-      <Grid container spacing={5} justifyContent="flex-start" alignItems="stretch" style={{width:"100%", margin:"auto" }}>
-        
-        <Grid item xs={12} sm={9} md={6} lg={5}>
-          <div style={{ paddingBottom: 10}}>
-            <div style={{textAlign:"center"}}>
-              <img src={`${BASE_URL}images/logo.jpeg`} width="92px" height="55px" style={{transform:"scale(1.3)"}} />
-              <div style={{ paddingBottom: 5 }}>
+
+      <Grid container spacing={2} justifyContent="center" alignItems="stretch" style={{width:"90%", margin:"auto"}}>
+        <Grid container justifyContent='center'  xs={12} sm={9} md={6} lg={5} style={{ paddingTop:"10px"}}>
+          <Grid container justifyContent='center' style={{ paddingBottom: 0}} lg={12} >
+            <Grid item style={{textAlign:"center"}} lg={4}>
+              <img src={`${BASE_URL}images/logo.jpeg`} width="92px" height="55px" style={{transform:"scale(1.5)", marginTop:"20px", marginBottom:"10px"}} />
+              <div>
                 <Typography variant="h5" style={{ color: '#0D3823', fontSize: 24, fontFamily: 'Playfair Display', fontWeight: 900, textTransform: 'capitalize', wordWrap: 'break-word' }}>
                   Indian Shop<br />
                 </Typography>
@@ -35,38 +35,40 @@ const FooterRenderer: React.FC = () => {
                   Milano
                 </Typography>
               </div>
-            </div>
-            <Typography>
-              Involved in the import and distribution of asian/indian food and grocery items of all types in milan, italia ,first indian groceries shop in milan
-            </Typography>
-          </div>
-          <Grid item  style={{marginBottom:"10px"}}>
-            <Typography variant="body1" style={{ fontWeight: 900 }}>
-              Address:
-            </Typography>
-            <Typography variant="body1">
-              Via Panfilo Castaldi, 32, 20124 Milano MI, Italy
-            </Typography>
+            </Grid>
+            <Grid item lg={7}>
+              <Typography style={{paddingBottom:"20px", paddingRight:"15px"}} align="justify" gutterBottom>
+                Involved in the import and distribution of asian/indian food and grocery items of all types in milan, italia ,first indian groceries shop in milan
+              </Typography>
+              <Grid item  style={{marginBottom:"10px"}} lg={12}>
+                <Typography variant="body1" style={{ fontWeight: 900 }}>
+                  Address:
+                </Typography>
+                <Typography variant="body1">
+                  Via Panfilo Castaldi, 32, 20124 Milano MI, Italy
+                </Typography>
+              </Grid>
+            </Grid>
           </Grid>
+          <Grid lg={12}>
           {/* <Box
-            style={{ width:"94%", border: "1px solid #1A3F2D", position:"absolute", bottom:"10px" }}
+            style={{ width:"100%", border: "1px solid #1A3F2D" }}
           /> */}
+          </Grid>
+          
         </Grid>
 
-        <Grid item xs={12} sm={9} md={6} lg={2}  >
+        <Grid item xs={12} sm={9} md={6} lg={2}>
           {/* <Typography style={{ fontSize: 16, fontFamily: 'Proxima Nova', paddingBottom: 20, fontWeight: 900, }}>Company</Typography> */}
-          <Typography style={{ marginBottom: 16 }} >Privacy Policy</Typography>
-          <Typography style={{ marginBottom: 16 }}>Terms of service</Typography>
-          <Typography style={{ marginBottom: 16 }}>Shipping policy</Typography>
-          <Typography style={{ marginBottom: 16 }}>Refund policy</Typography>
-          <Typography style={{ marginBottom: 16 }}>FAQs</Typography>
-          
-         
-          
+          <Typography style={{ marginBottom: 16, cursor:"pointer" }} >Privacy Policy</Typography>
+          <Typography style={{ marginBottom: 16, cursor:"pointer"  }}>Terms of service</Typography>
+          <Typography style={{ marginBottom: 16, cursor:"pointer"  }}>Shipping policy</Typography>
+          <Typography style={{ marginBottom: 16, cursor:"pointer"  }}>Refund policy</Typography>
+          <Typography style={{ marginBottom: 16, cursor:"pointer"  }}>FAQs</Typography>
         </Grid>
 
         <Grid item xs={12} sm={9} md={6} lg={3}>
-          <Typography style={{ marginBottom: 2 }}>Delivery partners</Typography>
+          <Typography style={{ marginBottom: 2, fontWeight: 900 }} variant="body1">Delivery partners</Typography>
           <div
           style={{
             display:"flex",
@@ -74,17 +76,17 @@ const FooterRenderer: React.FC = () => {
             alignItems:"center",
             marginBottom:"24px"
           }}>
-            <img src={`${BASE_URL}images/DHL.png`} alt="DHL" style={{ marginRight: 8, width: 50, height: 'auto' }} />
-            <img src={`${BASE_URL}images/BRT.png`} alt="BRT" style={{ marginRight: 8, width: 50, height: 'auto' }} />
-            <img src={`${BASE_URL}images/PosteItaliane.png`} alt="PosteItaliane" style={{ marginRight: 8, width: 50, height: 'auto' }} />
-            <img src={`${BASE_URL}images/SDA.png`} alt="sda" style={{ marginRight: 8, width: 50, height: 'auto' }} />
+            <img src={`${BASE_URL}images/DHL.png`} alt="DHL" style={{ marginRight: 12, width: 50, height: 'auto' }} />
+            <img src={`${BASE_URL}images/BRT.png`} alt="BRT" style={{ marginRight: 12, width: 50, height: 'auto' }} />
+            <img src={`${BASE_URL}images/PosteItaliane.png`} alt="PosteItaliane" style={{ marginRight: 12, width: 50, height: 'auto' }} />
+            <img src={`${BASE_URL}images/SDA.png`} alt="sda" style={{ marginRight: 12, width: 50, height: 'auto' }} />
             {/* Add more logos as needed */}
           </div>
-          <Typography style={{ marginBottom: 2 }}>Secured Payments</Typography>
+          <Typography style={{ marginBottom: 2, fontWeight: 900 }} variant="body1">Secured Payments</Typography>
           <div>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg" alt="Visa" style={{ marginRight: 8, width: 50, height: 50 }} />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg" alt="MasterCard" style={{ marginRight: 8, width: 50, height: 50 }} />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b5/PayPal.svg" alt="PayPal" style={{ marginRight: 8, width: 50, height: 50 }} />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg" alt="Visa" style={{ marginRight: 16, width: 50, height: 50 }} />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg" alt="MasterCard" style={{ marginRight: 16, width: 50, height: 50 }} />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b5/PayPal.svg" alt="PayPal" style={{ marginRight: 16, width: 50, height: 50 }} />
           </div>
           {/* <Box
             style={{ width:"94%", border: "1px solid #1A3F2D", position:"absolute", bottom:"10px" }}
