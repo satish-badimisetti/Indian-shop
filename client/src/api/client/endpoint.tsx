cf0bc83145914a33973a2e3f91f4383ac12db954
satish <badimisettisatish563@gmail.com>
2024-06-24 20:27:33 +0530

    added port

--- client/src/api/client/endpoint.tsx
+++ client/src/api/client/endpoint.tsx
@@ -1,6 +1,6 @@
 const apiConfig = {
 	// BASE_URL: "https://3.66.63.30:3000/",
-	BASE_URL: "http://3.79.130.16/",
+	BASE_URL: "http://3.79.130.16:3000/",
 	GET: {
 		GET_PRODUCTS: "/products/getproducts",
 		GET_CATEGORIES: "/products/allcategories",
